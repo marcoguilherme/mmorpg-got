876e4573188bb75da5f53ce822a599ac02091c3f
marcoguilherme <mgbm13@gmail.com>
2018-03-15 01:19:04 -0300

    Configurado a sessao e adicionado parametros do perfil do personagem

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -1,9 +1,10 @@
 module.exports.index = function(app, req, res){
-    if(req.session.autenticado){
-        res.render('jogo', {img_casa : req.session.casa});
-    }else{
+    if(req.session.autenticado !== true){
         res.render('index', { validacao :{}})
+        return;
     }
+
+    res.render('jogo', {img_casa : req.session.casa});
 }
 
 module.exports.sair = function(app, req, res){
--- app/models/JogoModel.js
+++ app/models/JogoModel.js
@@ -2,17 +2,19 @@ function JogoModel(connection){
     this._connection = connection();
 }
 
-JogoModel.prototype.gerarParametros(usuario){
+JogoModel.prototype.gerarParametros = function(usuario){
     this._connection.open( function(err, mongoclient){
         mongoclient.collection("jogo_parametros", function(err, collection){
             collection.insert({
+                usuario: usuario,
                 moeda: 15,
                 suditos: 10,
                 temor: Math.floor(Math.random() * 1000),
                 sabedoria: Math.floor(Math.random() * 1000),
                 comercio: Math.floor(Math.random() * 1000),
                 magia: Math.floor(Math.random() * 1000)
-            })
+            });
+            mongoclient.close();
         })
     })
 }
